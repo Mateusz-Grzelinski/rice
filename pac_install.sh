1b78db8a1216bb5e82259ddf53c86de98656a8f7
Mateusz Grzeliński <czubaka111@gmail.com>
2017-07-27 13:30:20 +0200

    installation script for a new maschine

---
+++ pac_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo pip install thefuck
+# pip install thefuck --upgrade
+sudo pacman -S --noconfirm blender vim cmake handbrake darktable yaourt virtualbox linux49-virtualbox-host-modules linux49-virtualbox-guest-modules fasd texmaker
+
+yaourt -S --noconfirm spotify clion pycharm-professional
+
+
+
