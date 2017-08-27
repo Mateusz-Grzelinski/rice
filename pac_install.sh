b28ca85156924fd331bafa7ab627d8f87e586a76
Mateusz Grzeliński <czubaka111@gmail.com>
2017-08-27 15:44:27 +0200

    More programs in pac_install

--- pac_install.sh
+++ pac_install.sh
@@ -2,9 +2,9 @@
 
 sudo pip install thefuck
 # pip install thefuck --upgrade
-sudo pacman -S --noconfirm blender vim cmake handbrake darktable yaourt virtualbox linux49-virtualbox-host-modules linux49-virtualbox-guest-modules fasd texmaker
+sudo pacman -S --noconfirm blender vim cmake handbrake darktable yaourt virtualbox linux49-virtualbox-host-modules linux49-virtualbox-guest-modules fasd texmaker unison grsync 
 
-yaourt -S --noconfirm spotify clion pycharm-professional
+yaourt -S --noconfirm spotify clion pycharm-professional unity-editor
 
 
 
