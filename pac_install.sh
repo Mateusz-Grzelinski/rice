79522ca0fce561cc95c9d553182cbf5c56967071
Mateusz Grzeliński <czubaka111@gmail.com>
2017-09-02 10:10:43 +0200

    Random changes

--- pac_install.sh
+++ pac_install.sh
@@ -2,7 +2,8 @@
 
 sudo pip install thefuck
 # pip install thefuck --upgrade
-sudo pacman -S --noconfirm blender vim cmake handbrake darktable yaourt virtualbox linux49-virtualbox-host-modules linux49-virtualbox-guest-modules fasd texmaker unison grsync 
+sudo pacman -S --noconfirm blender vim cmake handbrake darktable yaourt virtualbox linux49-virtualbox-host-modules linux49-virtualbox-guest-modules fasd texmaker unison grsync xclip dropbox python-numpy
+sudo pip install jedi
 
 yaourt -S --noconfirm spotify clion pycharm-professional unity-editor
 
