06ac9aef6cf9a75800a28987419e9e763e86fcea
Mateusz Grzeliński <czubaka111@gmail.com>
2017-09-02 11:57:12 +0200

    Minor changes. File .zlogin deleted

--- .keybindings.sh
+++ .keybindings.sh
@@ -1,3 +1,20 @@
+
+
+bindkey -s "" "fuck\n"
+bindkey -s "" "clear\n"
+
+# bring back delete key
+bindkey "\e[3~" delete-char
+
+
+# cd
+	# Custom cd
+	my_cd() {
+		cd $1;
+		ls | head -n 30 | ls;
+	}
+alias cd="my_cd"
+
 # up
 	function up_widget() {
 		BUFFER="cd .."
@@ -7,29 +24,29 @@
 	bindkey "" up_widget
 
 # git
-	function git_prepare() {
-		if [ -n "$BUFFER" ];
-			then
-				BUFFER="git add -A; git commit -m \"$BUFFER\" && git push"
-		fi
+	# function git_prepare() {
+	#     if [ -n "$BUFFER" ];
+	#         then
+	#             BUFFER="git add -A; git commit -m \"$BUFFER\" && git push"
+	#     fi
 
-		if [ -z "$BUFFER" ];
-			then
-				BUFFER="git add -A; git commit -v && git push"
-		fi
+	#     if [ -z "$BUFFER" ];
+	#         then
+	#             BUFFER="git add -A; git commit -v && git push"
+	#     fi
 				
-		zle accept-line
-	}
-	zle -N git_prepare
+	#     zle accept-line
+	# }
+	# zle -N git_prepare
 	# bindkey "^g" git_prepare
 
 # home
-	function goto_home() { 
-		BUFFER="cd ~/"$BUFFER
-		zle end-of-line
-		zle accept-line
-	}
-	zle -N goto_home
+	# function goto_home() { 
+	#     BUFFER="cd ~/"$BUFFER
+	#     zle end-of-line
+	#     zle accept-line
+	# }
+	# zle -N goto_home
 	# bindkey "^h" goto_home
 
 # Edit and rerun
@@ -38,7 +55,7 @@
 		zle accept-line
 	}
 	zle -N edit_and_run
-	# bindkey "^v" edit_and_run
+	bindkey "^v" edit_and_run
 
 # LS
 	function ctrl_l() {
@@ -61,4 +78,4 @@
 		zle end-of-line
 	}
 	zle -N add_sudo
-bindkey "^s" add_sudo
+    bindkey "^s" add_sudo
--- .zshrc
+++ .zshrc
@@ -1,30 +1,11 @@
 
 stty -ixon
 
-#
-# User configuration sourced by interactive shells
-#
-
-# source ~/antigen.zsh
 # Source zim
 if [[ -s ${ZDOTDIR:-${HOME}}/.zim/init.zsh ]]; then
   source ${ZDOTDIR:-${HOME}}/.zim/init.zsh
 fi
 
-#
-# User configuration sourced by interactive shells
-#
-
-eval "$(fasd --init auto posix-alias zsh-ccomp-install)"
-source ~/.dotfiles/.aliases 
-eval $(thefuck --alias)
-
-
-# plugins=(zsh-completions zsh-pip-completion)
-
-# HISTFILE=~/.zhistfile
-# HISTSIZE=1000
-# SAVEHIST=1000
 unsetopt autocd
 unsetopt extendedglob
 bindkey -e
@@ -34,27 +15,17 @@ setopt interactivecomments
 # allow to trunk file with >
 setopt clobber
 
-# bindkey '\e.' insert-last-word
-# End of lines configured by zsh-newuser-install
 # The following lines were added by compinstall
 zstyle :compinstall filename '/home/mat/.zshrc'
 
-# RPROMPT="%n"
-# export RPROMPT
-
-# completion
-# fpatch=(/home/mat/.antigen/bundles/srijanshetty/zsh-pip-completion $fpath)
 autoload -Uz compinit
 compinit
 # End of lines added by compinstall
 
+eval "$(fasd --init auto posix-alias zsh-ccomp-install)"
+eval $(thefuck --alias)
 
-# define shortcuts (widgets first)
-
-bindkey -s "" "fuck\n"
-bindkey -s "" "clear\n"
-
-# bring back delete key
-bindkey "\e[3~" delete-char
-
+source ~/.dotfiles/.aliases 
 source ~/.dotfiles/prompt_zsh.sh
+source ~/.dotfiles/.keybindings.sh
+
