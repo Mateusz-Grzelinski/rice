9deeb37e9982fd001337bf0c80db2f880949d607
Mateusz Grzeliński <czubaka111@gmail.com>
2017-09-09 13:38:57 +0200

    Minor cleanup

---
+++ inst.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+
+echo -e "This script will configure dotfiles for: \n\
+    zsh with zim (and nice prompt), \n\
+    bash (only aliases), \n\
+    vim with plugins, \n\
+    tmux (compatible with vim). "
+
+echo
+
+# backup current dotfiles
+while read -p "Make backup of dotfiles (zshrc, bashrc, vimrc, .vim folder, tmux.conf) (y/n)? " yn;
+do
+    case $yn in
+        t|T|tak|Tak|y|Y|yes|Yes) 
+            for file in ".zshrc" ".bashrc" ".vimrc" ".vim" ".tmux.conf" ".zimrc";
+            do
+                BACKUP_DIR="$HOME/.backup-dotfiles"
+                mkdir -p $BACKUP_DIR &&  test -f "$HOME/$file" && mv "$HOME/$file" "$BACKUP_DIR/$file"
+            done
+            echo "Backup done. Path: $BACKUP_DIR "
+            break;;
+        n|N|nie|Nie|no|No) 
+            echo "Dotfiles will be overriten";
+            break;;
+    esac
+done
+
+# linking configuration
+
+for file in "zshrc" "bashrc" "vimrc" "tmux.conf" "zimrc";
+do
+    echo "source ~/.dotfiles/$file" > $HOME/.$file;
+done
+
+# ----------------------------------
+# installing programs
+#-----------------------------------
+# check for exsistens of program fasd
+if [ command -v fasd &> /dev/null  ];
+then
+    # echo "Do you want fo install fasd? (y/n)"
+    while read -p "Do you want fo install fasd? (y/n) " yn ;
+    do
+        case $yn in
+        t|T|tak|Tak|y|Y|yes|Yes) 
+            echo "Installing fasd";
+            sudo pacman -S fasd;
+            break;;
+        n|N|nie|Nie|no|No) 
+            echo "Not installing";
+            echo "There will be problems with bashrc and zshrc. Edit them manually";
+            break;;
+        # *)
+        #     [ -z yn ] &&  echo "Invalid argument";;
+        esac
+    done
+else
+    echo "Program fasd is installed"
+fi
+
+echo
+
+echo "Installing vundle and other vim plugins..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2>/dev/null
+vim +PluginInstall +qall
+
+echo "Installing zsh"
+if [ command -v zsh &> /dev/null  ];
+then
+    sudo pacman -S zsh
+    echo "Installing zim..."
+else
+    echo "zsh is installed"
+fi
+
+echo "Instaling zim..."
+git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
+chsh -s /bin/zsh
