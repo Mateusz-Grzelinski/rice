3dea81cbbb3ad866fed410b07ddbe0e4ca231a36
Mateusz Grzeliński <czubaka111@gmail.com>
2017-09-19 14:30:35 +0200

    Removed unused commnets, fixed clear binding

--- keybindings.sh
+++ keybindings.sh
@@ -1,73 +1,24 @@
 
-
-#edit command on the fly in vim
-# autoload edit-command-line
-# zle -N edit-command-line
-# bindkey '^X^e' edit-command-line
-
-
+bindkey -e
 # bring back delete key
 bindkey "\e[3~" delete-char
 bindkey "^[[A" history-beginning-search-backward
 bindkey "^[[B" history-beginning-search-forward
+# bash style comment
+bindkey '\e#' pound-insert
+setopt interactivecomments
 
+# clear screen with alt-l
+bindkey -s 'l' 'clear\n' 
 
-# cd
-	# Custom cd
-    # when going up run ls, when down (..) oly pwd
-	function my_cd() {
-        if [ -z "$1" ]; 
-        then 
-            cd
-        elif [ "$1" = ".." ]; 
-        then 
-            cd ..; 
-            pwd;
-	    else
-            cd $1;
-            ls --color=always --format=vertical | head -n 5 ;
-            echo "And more..."
-        fi
-    }
-    bindkey -s "" "cd ..\n"
-    # zle -N my_cd
-    # bindkey "" "my_cd .."
-
-# alias cd="my_cd"
 
 # up
-	# function up_widget() {
-	#     BUFFER="cd .."
-	#     zle accept-line
-	# }
-	# zle -N up_widget
-	# bindkey "" up_widget
-
-# git
-	# function git_prepare() {
-	#     if [ -n "$BUFFER" ];
-	#         then
-	#             BUFFER="git add -A; git commit -m \"$BUFFER\" && git push"
-	#     fi
-
-	#     if [ -z "$BUFFER" ];
-	#         then
-	#             BUFFER="git add -A; git commit -v && git push"
-	#     fi
-				
-	#     zle accept-line
-	# }
-	# zle -N git_prepare
-	# bindkey "^g" git_prepare
-
-# home
-	# function goto_home() { 
-	#     BUFFER="cd ~/"$BUFFER
-	#     zle end-of-line
-	#     zle accept-line
-	# }
-	# zle -N goto_home
-	# bindkey "^h" goto_home
+    function up_widget() {
+        BUFFER="cd .."
+        zle accept-line
+    }
+    zle -N up_widget
+bindkey "" up_widget
 
 # Edit and rerun
 	function edit_and_run() {
@@ -99,3 +50,9 @@ bindkey "^[[B" history-beginning-search-forward
 	}
 	zle -N add_sudo
     bindkey "^s" add_sudo
+
+# Fasd
+    bindkey '^X^A' fasd-complete    # C-x C-a to do fasd-complete (fils and directories)
+    bindkey '^X^F' fasd-complete-f  # C-x C-f to do fasd-complete-f (only files)
+    bindkey '^X^D' fasd-complete-d  # C-x C-d to do fasd-complete-d (only directories)
+
