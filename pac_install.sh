fbad14cb1435fd0af08c278792094339417910c9
Mateusz Grzeliński <czubaka111@gmail.com>
2017-09-22 15:15:13 +0200

    Added dependencies for programing suite

--- pac_install.sh
+++ pac_install.sh
@@ -2,10 +2,11 @@
 
 echo "Read the script!! Many programs will be installed"
 read
-sudo pacman -S --noconfirm blender vim cmake handbrake darktable yaourt virtualbox linux49-virtualbox-host-modules linux49-virtualbox-guest-modules fasd texmaker unison grsync xclip dropbox python-numpy borg pyqt5
-sudo pip3 install jedi
+sudo pacman -S --noconfirm blender vim cmake ctags handbrake darktable yaourt virtualbox linux49-virtualbox-host-modules linux49-virtualbox-guest-modules fasd texmaker unison grsync xclip dropbox python-numpy borg pyqt5 kodi hplip neovim bleachbit codeblocks shellcheck clang
 
-yaourt -S --noconfirm spotify clion pycharm-professional unity-editor
+sudo pip3 install jedi pytest nose numpy
+
+yaourt -S --noconfirm spotify clion pycharm-professional unity-editor chrome
 
 
 
